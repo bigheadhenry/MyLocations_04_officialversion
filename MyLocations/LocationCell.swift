943f407e0e04ce6f36f327214c09817b9ed3e192
Jing Sun <bigheadhenry@gmail.com>
2015-02-16 08:46:50 +0800

    My first github project
    I’m learning Swift and this is my first github project.

---
+++ MyLocations/LocationCell.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class LocationCell: UITableViewCell {
+  @IBOutlet weak var descriptionLabel: UILabel!
+  @IBOutlet weak var addressLabel: UILabel!
+
+  func configureForLocation(location: Location) {
+    if location.locationDescription.isEmpty {
+      descriptionLabel.text = "(No Description)"
+    } else {
+      descriptionLabel.text = location.locationDescription
+    }
+    
+    if let placemark = location.placemark {
+      addressLabel.text =
+        "\(placemark.subThoroughfare) \(placemark.thoroughfare)," +
+        "\(placemark.locality)"
+    } else {
+      addressLabel.text = String(format:"Lat: %.8f, Long: %.8f", location.latitude, location.longitude)
+    }
+  }
+}
